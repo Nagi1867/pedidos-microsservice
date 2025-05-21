9f695c0fe03472950a5aa704f3fc00eefba4574e
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-05-21 14:53:54 -0300

    swagger

---
+++ pedidos-api/src/main/java/com/example/pedidos/api/entity/Produto.java
@@ -0,0 +1,56 @@
+package com.example.pedidos.api.entity;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Produto {
+    private UUID id = UUID.randomUUID();
+    private String nome;
+    private Double valor;
+
+    public Produto() {
+    }
+
+    public Produto(UUID id, String nome, Double valor) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id) && Objects.equals(nome, produto.nome) && Objects.equals(valor, produto.valor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, valor);
+    }
+}
---
+++ pedidos-api/src/main/java/com/example/pedidos/api/entity/enums/Status.java
@@ -0,0 +1,5 @@
+package com.example.pedidos.api.entity.enums;
+
+public enum Status {
+    EM_PROCESSAMENTO, PROCESSADO;
+}
