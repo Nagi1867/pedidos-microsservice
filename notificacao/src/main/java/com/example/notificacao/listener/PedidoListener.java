0f5a854db8174e566c428cc514a7db27b6176132
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-05-22 17:00:25 -0300

    terminando o mailhog

--- notificacao/src/main/java/com/example/notificacao/listener/PedidoListener.java
+++ notificacao/src/main/java/com/example/notificacao/listener/PedidoListener.java
@@ -1,17 +1,22 @@
 package com.example.notificacao.listener;
 
 import com.example.notificacao.entity.Pedido;
+import com.example.notificacao.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PedidoListener {
     private Logger logger = LoggerFactory.getLogger(PedidoListener.class);
+    @Autowired
+    private EmailService emailService;
 
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-notificacao")
     public void enviarNotificacao(Pedido pedido) {
-        logger.info("Notificação gerada: %s", pedido.toString());
+        emailService.enviarEmail(pedido);
+        logger.info("Notificação gerada: {}", pedido.toString());
     }
 }
