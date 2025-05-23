892af1c6220fd2760cbbd2f327786a70bc7539ca
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-05-23 16:22:50 -0300

    repositories e rabbitmq

---
+++ processador/src/main/java/com/example/pedidos/processador/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.pedidos.processador.repository;
+
+import com.example.pedidos.processador.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+}
